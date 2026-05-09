cf44c9df14d91f73e010dacd2fefe7037a7f3574
Tarrah4Tammwe <133949570+Tarrah4Tammwe@users.noreply.github.com>
2026-05-09 11:21:28 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,35 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Canonical-URL',
+            value: 'https://www.overstaycheck.com/:path*',
+          },
+        ],
+      },
+    ];
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://www.overstaycheck.com/:path*',
+        basePath: false,
+        permanent: true,
+        has: [
+          {
+            type: 'host',
+            value: 'overstaycheck.com',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 export default nextConfig
